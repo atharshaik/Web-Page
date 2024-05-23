53522683c56d5145ed93eea1096e9368f5ef77d2
atharshaik <shaikathar.p@gmail.com>
2024-05-23 16:55:44 +0530

    modified

--- main.js
+++ main.js
@@ -2,4 +2,22 @@ const name = 'Shaik';
 const age = 22;
 
 // concatenation
-console.log('My name is ' + name + 'and  i am ' +age + 'years old');
+console.log('My name is ' + name + ' and  i am ' +age + 'years old');
+
+const s = 'My, Name , Is , Shaik, Athar, Pasha'
+console.log(s.split(','))
+
+
+// Arrays - variables that hold multiple values
+
+const numbers = new Array(1,2,3,4,5);
+console.log(numbers);
+
+const fruits = ['apple', 'oranges'];
+
+console.log(fruits)
+x = 20; // 'x' is reassigned to a new value, but 10 itself remains unchanged
+
+let y = "hello";
+y = y + " world"; // creates a new string "hello world"
+console.log(y)
