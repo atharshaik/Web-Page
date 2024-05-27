a518fad1511c6af610fcd0a13a5cc4c602d8d633
atharshaik <shaikathar.p@gmail.com>
2024-05-27 17:28:50 +0530

    modified

--- main1.js
+++ main1.js
@@ -1,10 +1,45 @@
+// changing colo of btn 
 const btn =  document.querySelector('.btn');
 btn.style.background = 'black';
 
-btn.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log('click')
-})
+// changing backround color using this qurey 
 
+// btn.addEventListener('click', function(e){
+//     e.preventDefault();
+//     console.log('e')
+//     document.querySelector('#my-form').style.background = '#ccc';
+    
+// });
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    if(nameInput.value === '' || emailInput.value === ''){
+        // alert('please enter fields')
+        msg.classList.add('error');
+        msg.innerHTML = 'please enter all fields';
+
+
+        setTimeout(() => msg.remove, 1000);
+    }else{
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        //c;ear fields
+        nameInput.value = '';
+        emailInput.value = '';
+        
+    }
+    
+}
 
 
