75cb9b83ad8fb96087c6b81a1fe4067a79f7e75e
atharshaik <shaikathar.p@gmail.com>
2024-05-28 18:32:24 +0530

    modified

--- dom.js
+++ dom.js
@@ -1,1 +1,6 @@
-// console.dir(document);
+// console.dir(document);;
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 'shaik';
+console.log(document.getElementById('header'));
