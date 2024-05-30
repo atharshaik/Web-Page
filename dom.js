c1aabe4d40bac6014e86c67fc33e3d076839cfe6
mratharshaik <shaikathar.p@gmail.com>
2024-05-30 22:18:05 +0530

    modified

--- dom.js
+++ dom.js
@@ -9,3 +9,29 @@ header.textcontent = 'hello world';
 header.innertext = 'hello';
 var headerBase = document.getElementById('header');
 headerBase.style.borderBottom = 'solid #ccc 3px';
+
+// GET ELEMENT BY CLASS
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'hello' ;
+items[2].style.fontWeight = 'bold';
+
+for(var i=0; i<items.length; i++){
+    items[i].style.backgrounColor = '#f4f4f4';
+}
+
+// get lement by tagname
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'hello' ;
+li[2].style.fontWeight = 'bold';
+
+for(var i=0; i<li.length; i++){
+    li[i].style.backgrounColor = '#f4f4f4';
+}
+
+
